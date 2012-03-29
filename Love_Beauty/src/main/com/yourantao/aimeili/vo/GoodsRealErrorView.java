85d61a4967c446a8fb5fb9ddf9e9b745a0f0e7d2
jiangjianliang <jjl_19@sohu.com>
2012-03-29 14:30:33 +0800

    Signed-off-by: jiangjianliang <jjl_19@sohu.com>

---
+++ Love_Beauty/src/main/com/yourantao/aimeili/vo/GoodsRealErrorView.java
@@ -0,0 +1,26 @@
+package main.com.yourantao.aimeili.vo;
+
+public class GoodsRealErrorView {
+	private int providerId;
+	private int goodsRealId;
+	private int errorCode;
+	public int getProviderId() {
+		return providerId;
+	}
+	public void setProviderId(int providerId) {
+		this.providerId = providerId;
+	}
+	public int getGoodsRealId() {
+		return goodsRealId;
+	}
+	public void setGoodsRealId(int goodsRealId) {
+		this.goodsRealId = goodsRealId;
+	}
+	public int getErrorCode() {
+		return errorCode;
+	}
+	public void setErrorCode(int errorCode) {
+		this.errorCode = errorCode;
+	}
+	
+}
